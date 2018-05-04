b96e0bec6e10e594b2e44ad80923934158a370de
Ji Yim <gsooyim@gmail.com>
2018-05-04 23:00:44 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(key, value)
+  def initialize()
     key.send(value)
   end
   
